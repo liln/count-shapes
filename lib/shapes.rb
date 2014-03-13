0fc3ab72dce6724bc94fe1c6640fb6ba91ee98d1
Lillian Ng <lillian.k.ng@gmail.com>
2014-03-12 17:57:12 -0700

    fixed typo

--- lib/shapes.rb
+++ lib/shapes.rb
@@ -14,8 +14,8 @@ end
 
 def traverse_shape(display, x, y, n)
   return unless x >= 0 && x < n && y >= 0 && y < n
-  return unless @display[x,y] == 1
-  @display[x,y] = 0
+  return unless display[x,y] == 1
+  display[x,y] = 0
   traverse_shape display, x-1, y, n
   traverse_shape display, x, y-1, n
   traverse_shape display, x, y+1, n
